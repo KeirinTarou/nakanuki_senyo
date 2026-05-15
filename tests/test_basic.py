7adb8e7a92f7f61ba297c23f9eaf3b2b91d87af7
Keirin Tarou <seoo_japan_keirinoh@yahoo.co.jp>
2026-05-16 08:59:00 +0900

    テスト準備完了
    - `src`, `src.nakanuki_core`, `src_nakanuki_gui`のimport -> PASS
    - 変更・追加したファイル
        - modified:   .gitignore
        - new file:   pytest.ini
        - new file:   src/__init__.py
        - new file:   tests/__init__.py
        - new file:   tests/test_basic.py

---
+++ tests/test_basic.py
@@ -0,0 +1,16 @@
+# TODO:
+#   - srcモジュールが読める
+#   - nakanuki_coreモジュールが読める
+#   - nakanuki_guiモジュールが読める
+
+def test_import_module_src():
+    """ srcモジュールが読める"""
+    import src
+
+def test_import_module_src_nakanuki_core():
+    """ src.nakanuki_coreモジュールが読める"""
+    import src.nakanuki_core
+
+def test_import_module_src_nakanuki_gui():
+    """ src.nakanuki_guiモジュールが読める"""
+    import src.nakanuki_gui
