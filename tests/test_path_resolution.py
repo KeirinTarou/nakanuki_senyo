3a31063ae7b7975146a1564ee0902932cd7796b4
seo tomohiro <seoo_japan_keirinoh@yahoo.co.jp>
2026-06-06 15:12:46 +0900

    test_path_resolution: コメントメンテ

--- tests/test_path_resolution.py
+++ tests/test_path_resolution.py
@@ -2,6 +2,8 @@ import sys
 
 import config
 
+# 実行時のパス解決保証テスト
+
 # TODO: 
 #   ✅- コマンドライン実行時に`resources`フォルダのパス解決ができる
 #   ✅- EXE実行時に`resources`フォルダのパス解決ができる
@@ -12,6 +14,7 @@ def test_path_resolution_resource_path_normal(monkeypatch):
     assert config.resource_path("a/b.txt") == config.RESOURCES_DIR / "a/b.txt"
 
 def test_path_resolution_resource_path_meipass(monkeypatch, tmp_path):
+    """ EXE実行時に`resources`フォルダのパス解決ができる"""
     meipass = tmp_path / "bundle"
     meipass.mkdir()
     monkeypatch.setattr(sys, "_MEIPASS", str(meipass), raising=False)
